595bd13d45d1ff7fc6b3b5ca78a17e2e0af75eb7
Kevin <kevinrk29@hotmail.com>
2018-07-14 19:15:21 -0400

    The header file

---
+++ header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#define SIZE 1000
+
+void signUp (char username [SIZE], char password [SIZE], char name [SIZE]);
+int logIn (char username [SIZE], char password [SIZE]);
+void control();
+
+
+#endif
